d3417c30c2d6d04b693c1491b576e9e9a5ca2429
lohrd <57124913+lohrd@users.noreply.github.com>
2020-03-18 21:23:15 -0400

    Initialize Website Portfolio

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom'
+
+
+const App = (props) => {
+    return (
+        <div>
+            <p>this is example text</p>
+        </div>
+    )
+}
+
+ReactDOM.render(
+    <App></App>,
+    document.getElementById('root')
+    );
+  
