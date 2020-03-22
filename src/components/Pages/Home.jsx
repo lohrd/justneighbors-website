1557e0bba527a4918149900bdff359124bc74d46
lohrd <57124913+lohrd@users.noreply.github.com>
2020-03-22 00:06:24 -0400

    Add Pages

---
+++ src/components/Pages/Home.jsx
@@ -0,0 +1,21 @@
+import * as React from './node_modules/react';
+import '../styles.css';
+import Image from '../images/monterrey.jpeg'
+
+const backgroundImage = require('../images/monterrey.jpeg');
+const backgroundStyle = {
+    backgroundImage: `url(${Image})`,
+    backgroundPosition: 'center center',
+    backgroundRepeat: 'no-repeat',
+    backgroundAttachment: 'fixed',
+    backgroundSize: 'cover',
+    backgroundColor: '#040405'
+}
+
+const Home = () => {
+    return (
+        <div style={backgroundStyle}></div>
+    )
+}
+
+export default Home;
