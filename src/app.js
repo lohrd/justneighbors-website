63e887dbd1d590a45e6b80c0777668429fc4e700
lohrd <57124913+lohrd@users.noreply.github.com>
2020-03-23 20:44:50 -0400

    Organization

--- src/app.js
+++ src/app.js
@@ -1,27 +1,20 @@
 import * as React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Home from './components/Pages/Home'
-import Image from './images/monterrey.jpeg'
-
-// const backgroundImage = require('./images/monterrey.jpeg');
-// const backgroundStyle = {
-//     backgroundImage: `url(${Image})`,
-//     backgroundPosition: 'center center',
-//     backgroundRepeat: 'no-repeat',
-//     backgroundAttachment: 'fixed',
-//     backgroundSize: 'cover',
-//     backgroundColor: '#040405'
-// }
 
 
-const test = "hello world!"
 
 const App = () => {
     return (
-        <div className="background" style={{backgroundImage: `url(${Image})`}} >
+        <Router>
+            <Switch>
+                <Route></Route>
+                <Route></Route>
+                <Route></Route>
+            </Switch>
 
-            <Home></Home>
-        </div>
+        </Router>
+        <Home></Home>
     )
 }
 
