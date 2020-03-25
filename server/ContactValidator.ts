9bedef7b9deca46b33e11269c55b03eb23d933bf
lohrd <57124913+lohrd@users.noreply.github.com>
2020-03-24 22:35:48 -0400

    Add ContactValidator
    - Joi middleware for express server requests

---
+++ server/ContactValidator.ts
@@ -0,0 +1,11 @@
+import * as Joi from "joi";
+class ContactValidator {
+  createContact= {
+    body: {
+      name: Joi.string().required(),
+      email: Joi.number().integer().positive().required(),
+      request: Joi.string().required()
+    }
+  }
+}
+export default new ContactValidator();
