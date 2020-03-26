f65fb5e943bf4f2d30a1099267e9db9c3d663bb3
lohrd <57124913+lohrd@users.noreply.github.com>
2020-03-25 23:51:04 -0400

    Add functionality to inversify.config
    - dependency injection module

---
+++ server/inversify.config.ts
@@ -0,0 +1,17 @@
+import { ContainerModule } from "inversify";
+import TYPES from "./types";
+import ContactDao from "./ContactDao";
+import { IContactDao } from "./ContactDao";
+import { Repository, getRepository } from "typeorm";
+import Contact from "./Contact";
+
+import "../tiers/presentation-layer/controllers/ContactController" // auto-binding courtesy of the utils
+
+export const bindings = new ContainerModule(bind => {
+
+    bind<IContactDao>(TYPES.IContactDao).to(ContactDao);
+    bind<Repository<Contact>>(TYPES.IContactRepository).toDynamicValue(() => {
+        return getRepository(Contact);
+    }).inRequestScope();
+});
+
