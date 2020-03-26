ce35d1d5fbf4cd78c36b490684eb68b80b76c598
lohrd <57124913+lohrd@users.noreply.github.com>
2020-03-26 03:23:27 -0400

    Add About Page

---
+++ src/components/Pages/About.jsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import '../../styles.css';
+
+
+
+const About = (props) => {
+    return (
+        <div className="background" style={{backgroundImage: `url(${props.aboutImage})`}} >
+
+        </div>
+    )
+}
+
+export default About;
