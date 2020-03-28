e61362c14c25657ef3894d00e238f69d85cfee17
lohrd <57124913+lohrd@users.noreply.github.com>
2020-03-28 16:05:04 -0400

    Initialize Material UI Theme

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom'
 import App from './App'
+import { ThemeProvider } from '@material-ui/core'
+import theme from './components/Layout/Theme'
 
 ReactDOM.render(
-    <App/>,
+    <ThemeProvider theme={theme}>
+        <App/>
+    </ThemeProvider>,
     document.getElementById('root')
     );
   
