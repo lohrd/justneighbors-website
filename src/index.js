cd0750cc2bcd9120824f4ffc67208683843555b5
lohrd <57124913+lohrd@users.noreply.github.com>
2020-03-29 01:04:38 -0400

    misc. fixes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom'
+import '@fortawesome/fontawesome-free/css/all.min.css'; 
+import 'bootstrap-css-only/css/bootstrap.min.css'; 
+import 'mdbreact/dist/css/mdb.css'
 import App from './App'
 import { ThemeProvider } from '@material-ui/core'
 import theme from './components/Layout/Theme'
