033b1825a2ee3f6ce736a6947dd192fe0a265d9f
lohrd <57124913+lohrd@users.noreply.github.com>
2020-03-29 01:03:29 -0400

    Demo Mat UI

--- src/components/Layout/Theme.js
+++ src/components/Layout/Theme.js
@@ -1,4 +1,5 @@
 import { createMuiTheme } from '@material-ui/core';
+// import 'typeface-league-spartan'
 
 const theme = createMuiTheme({
     palette: {
--- src/components/Pages/Contact.jsx
+++ src/components/Pages/Contact.jsx
@@ -1,9 +1,42 @@
 import * as React from 'react';
 import ContactSDK from '../../ContactSDK'
-import { Button } from '@material-ui/core'
+import { Button, Container, TextField, Grid, TextareaAutosize} from '@material-ui/core'
+import { makeStyles, styled } from '@material-ui/core/styles'
 
+const useStyles = makeStyles((theme) => ({
+    container: {
+        minHeight: "100vh"
+      },
+    root: {
+        display: 'flex',
+        height: '41px',
+        width: '392px',
+        justifyContent: 'center',
+        alignItems: 'center',
+        '& .MuiTextField-root': {
+            margin: theme.spacing(1),
+            width: '25ch',
+        },
+     },
+     input: {
+        color: 'white',
+        backgroundColor: 'grey',
+        opacity: '0.75',
+        width: '392px'
+     }
+  }));
 
+const MyButton = styled(Button)({
+    background: 'grey',
+    border: 0,
+    borderRadius: 3,
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    color: 'white',
+    height: 48,
+    padding: '0 30px'
+  });
 const Contact = (props) => {
+    const classes = useStyles();
     const [contact, setContact] = React.useState({})
     const [name, setName] = React.useState("name");
     const [email, setEmail] = React.useState("email");
@@ -25,11 +58,16 @@ const Contact = (props) => {
     
       }, [])
     return (
-        <div className="background" style={{backgroundImage: `url(${props.contactImage})`}}>            
-            <input onChange={addName} value={name}></input>
-            <input onChange={addEmail} value={email}></input>
-            <input onChange={addMessage} value={message}></input>
-            <Button onClick={newContactRequest}>Submit</Button>
+        <div className="background" style={{backgroundImage: `url(${props.contactImage})`}}>   
+
+            <Grid container spacing={4} align="center" alignItems="center" justify="center" direction="column" className={classes.container}> 
+                    <Grid item><TextField className={classes.root} variant="filled" onChange={addName} value={name}  InputProps={{className: classes.input}}></TextField></Grid>
+                    <Grid item><TextField className={classes.root} variant="filled" onChange={addEmail} value={email} InputProps={{className: classes.input}}></TextField></Grid>
+                    <Grid item><TextField margin='normal' className={classes.root} variant="filled" onChange={addMessage} value={message} multiline rows="4" InputProps={{className: classes.input}}></TextField></Grid>
+                    <Grid item><MyButton onClick={newContactRequest}>Submit</MyButton></Grid>
+            </Grid>
+        
+
         </div>
     )
 }
